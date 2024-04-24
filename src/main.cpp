ee56e4b5695a6895ce54b401cc60dcb654c1f0fa
taho7766 <wjtw9802@icloud.com>
2024-04-23 21:48:01 -0600

    connected to external camera source and added frame display

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
+#include <thread>
+#include <ctime>
 #include <opencv2/highgui.hpp>
 
+void draw_fps(cv::Mat &frame, int fps) {
+  cv::putText(frame, std::to_string(fps), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 255, 255), 1);
+}
+
 int main() {
-  cv::VideoCapture cap(0);
+  cv::VideoCapture cap(1);
   if(!cap.isOpened()){
     return -1;
   }
   cv::Mat frame;
   cv::namedWindow("TESTING", cv::WINDOW_AUTOSIZE);
+    int frame_counter = 0;
+    int tick = 0;
+    int fps;
+    std::time_t time_begin = std::time(0);
+
   while(true) {
     cap >> frame;
     cv::flip(frame, frame, 1);
@@ -17,9 +28,17 @@ int main() {
       std::cerr << "Failed to capture frame" << std::endl;
       continue;
     }
-    
-    cv::imshow("TESTING" ,frame);
 
+    frame_counter++;
+
+    std::time_t time_now = std::time(0) - time_begin;
+    if(time_now - tick >= 1){
+      tick++;
+      fps = frame_counter;
+      frame_counter = 0;
+    }
+    draw_fps(frame, fps);
+    cv::imshow("TESTING" ,frame);
     int key = cv::waitKey(1);
     if((key & 0xFF) == 27 || (key & 0xFF) == 'q') {
       break;
