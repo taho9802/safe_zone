25f2cad9cdcee69b43b3eee303c1da6ebed28eb0
taho7766 <wjtw9802@icloud.com>
2024-05-01 20:09:54 -0600

    Added Zone_Manager class and implementation

--- include/zones/Zone_Manager.hpp
+++ include/zones/Zone_Manager.hpp
@@ -2,20 +2,27 @@
 #define ZONE_MANAGER_HPP
 
 #include <vector>
+#include <opencv2/core.hpp>
 #include "Zone.hpp"
 
 class Zone_Manager {
   private:
-    std::vector<Zone> zones;
+    std::vector<Zone*> zones;
+    std::vector<std::vector<cv::Point>> zone_polygons;
+    int total_created_zones;
+    cv::Scalar color;
+
+    int zone_finder(cv::Point pt);
+    bool validate_zones();
+    int create_zone_id();
   public:
     Zone_Manager();
-    void add_zone(Zone zone);
-    Zone get_zone();
+    Zone_Manager(cv::Scalar color);
+    void add_zone(std::vector<cv::Point> vertices);
+    Zone* get_zone(cv::Point pt);
     void delete_zone();
-    void handle_mouse_click(int event, int x, int y, int flags);
-    void handle_key_click(int key);
     void draw_zones(cv::Mat &frame);
-    int create_zone_id();
+    void destroy_all_zones();
 };
 
 #endif // ZONE_MANAGER_HPP
